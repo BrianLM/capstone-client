5c751426a78ea77d19b7d9ed06832b221e982ce2
BrianLM <brian.distefano@libertymutual.com>
2017-11-25 10:28:28 -0500

    Add base Explore
    Add and test routing to explore components

--- src/app/explore/exp-routing.module.ts
+++ src/app/explore/exp-routing.module.ts
@@ -1,10 +1,34 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+
+import { IndexComponent } from '../explore/index/index.component'
+import { AreaComponent } from '../explore/area/area.component'
+import { ExploreComponent } from '../explore/explore.component'
+
+const routes: Routes = [
+  {
+    path: 'explore',
+    component: ExploreComponent,
+    children: [
+      {
+        path: 'index',
+        component: IndexComponent
+      },
+      {
+        path: 'area',
+        component: AreaComponent
+      }
+    ]
+  }
+]
 
 @NgModule({
   imports: [
-    CommonModule
+    RouterModule.forRoot(routes)
   ],
-  declarations: []
+  exports: [
+      RouterModule
+  ]
 })
 export class ExpRoutingModule { }
--- src/app/explore/explore.module.ts
+++ src/app/explore/explore.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { IndexComponent } from './index/index.component';
 import { AreaComponent } from './area/area.component';
 import { ExploreComponent } from './explore.component';
+import { ExpRoutingModule} from './exp-routing.module'
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    ExpRoutingModule
   ],
   declarations: [IndexComponent, AreaComponent, ExploreComponent]
 })
