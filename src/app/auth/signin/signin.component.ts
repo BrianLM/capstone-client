5ff94aae025228338a125663d51b149bb1fc75e4
BrianLM <brian.distefano@libertymutual.com>
2017-11-29 10:19:14 -0500

    Email validation style testing
    Change input types to email and password

--- src/app/auth/signin/signin.component.ts
+++ src/app/auth/signin/signin.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth/auth.service'
+import { EmailValidator } from '@angular/forms';
 
 @Component({
   selector: 'app-signin',
