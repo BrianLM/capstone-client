e7c6790b47600628f8d7e03d070d44b4614dada4
BrianLM <brian.distefano@libertymutual.com>
2017-11-28 20:18:02 -0500

    Adds User Profile service function
    Adds Get User Profile on route changes
    Adds style to anchors globally

--- src/app/services/profile/profile.service.ts
+++ src/app/services/profile/profile.service.ts
@@ -1,24 +1,36 @@
 import { Injectable, EventEmitter } from '@angular/core';
 import { environment } from '../../../environments/environment';
 import { Http } from '@angular/http';
-import { Router } from '@angular/router';
+import { Router, NavigationEnd } from '@angular/router';
 
 import { AuthService } from '../auth/auth.service'
 
 @Injectable()
 export class ProfileService {
-  profileUpdated = new EventEmitter()
 
-  profile: any
   constructor(
     private http: Http,
     private router: Router,
     private auth: AuthService
-  ) { }
+  ) {
+    router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.getProfile()
+      }
+    })
+  }
 
   getProfile() {
     let config = {}
     config['headers'] = { Authorization:'Token token=' + localStorage.getItem('token')}
-
+    this.http.get(environment.apiOrigin + '/user_profiles/' + localStorage.getItem('id'), config)
+      .subscribe(
+        response => {
+          const user = JSON.parse(response['_body']).user_profile
+          for (let key in user) {
+            localStorage.setItem(key, user[key])
+          }
+        }
+      )
   }
 }
